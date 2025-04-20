fcc29c061b571b495ac0d14f284f5af75cc37033
Jerry0666 <zzherui54@gmail.com>
2025-04-20 15:32:46 +0800

    Modify login and register page.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,17 +1,24 @@
 
-console.log(`This is a test!`);
+function show_hide() {
+    var login = document.getElementById("container1");
+    var signup = document.getElementById("container2");
+    var copyright = document.getElementById("copyright");
 
-const Username = document.getElementById(`name`);
-const SubmitBtn = document.getElementById(`SubmitBtn`);
+    if (login.style.display === "none") {
+        login.style.display = "block";  //lonin出現
+        document.getElementById("username").value="";
+        document.getElementById("password").value="";
+        signup.style.display = "none";  //signup消失
+        copyright.style.margin = "200px 0px 0px 0px";
+    } else {
+        login.style.display = "none";   //login消失
+        signup.style.display = "block"; //signup出現
+        signup.style.visibility="visible";
+        copyright.style.margin = "200px 0px 0px 0px";
 
-SubmitBtn.onclick = function(event) {
-    const UsernameStr = Username.value;
-    console.log(UsernameStr);
-    fetch("/api", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ name: UsernameStr})
-    });
-};
+        document.getElementById("fullname").value="";
+        document.getElementById("username2").value="";
+        document.getElementById("password2").value="";
+        document.getElementById("comfirm_password").value="";
+    }
+}
